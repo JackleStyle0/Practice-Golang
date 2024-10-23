ed7413be30f4c6dd714fa26d8be7320389c2d184
wacharat thongsomboon <wacharatthongsomboon@wacharats-MacBook-Pro.local>
2024-10-23 16:55:46 +0700

    Create migration file with goose

---
+++ db/migration/20241023093024_init_schema.sql
@@ -0,0 +1,43 @@
+-- +goose Up
+-- +goose StatementBegin
+SELECT 'up SQL query';
+CREATE TABLE "follows" (
+  "following_user_id" integer,
+  "followed_user_id" integer,
+  "created_at" timestamp
+);
+
+CREATE TABLE "users" (
+  "id" integer PRIMARY KEY,
+  "username" varchar,
+  "role" varchar,
+  "created_at" timestamp
+);
+
+CREATE TABLE "posts" (
+  "id" integer PRIMARY KEY,
+  "title" varchar,
+  "body" text,
+  "user_id" integer,
+  "status" varchar,
+  "created_at" timestamp
+);
+
+COMMENT ON COLUMN "posts"."body" IS 'Content of the post';
+
+ALTER TABLE "posts" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+
+ALTER TABLE "follows" ADD FOREIGN KEY ("following_user_id") REFERENCES "users" ("id");
+
+ALTER TABLE "follows" ADD FOREIGN KEY ("followed_user_id") REFERENCES "users" ("id");
+
+
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+SELECT 'down SQL query';
+DROP TABLE IF EXISTS posts;
+DROP TABLE IF EXISTS follows;
+DROP TABLE IF EXISTS users;
+-- +goose StatementEnd
